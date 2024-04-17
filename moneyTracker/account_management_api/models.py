d065bd5bd3c1d1efaabebae22cfbcd9c32c54ddf
saurav thakur <sauravthakur188@gmail.com>
2024-04-17 13:17:13 +0545

    [ADD]models added

--- moneyTracker/account_management_api/models.py
+++ moneyTracker/account_management_api/models.py
@@ -1,3 +1,81 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
+
+class Account(models.Model):
+    user_id = models.ForeignKey("auth.User", verbose_name=_("Owner"), on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(_("Description"))
+    balance = models.FloatField(_("Balance"))
+    show_card = models.BooleanField(_("Show Card"))
+    show_pie = models.BooleanField(_("Show Pie Chart"))
+    show_line = models.BooleanField(_("Show in Line Chart"))
+    theme_color_hash = models.CharField(_("Theme Color"), max_length=7)
+    theme_icon_fa_class = models.CharField(_("Theme Icon FA class"), max_length=50)
+
+    SAVINGS = "SVG"
+    CHECKING = "CHK"
+    INVESTMENT = "INV"
+    ACCOUNT_TYPE_CHOICE = {
+        SAVINGS: "Savings",
+        CHECKING: "Checking",
+        INVESTMENT: "Investment",
+    }
+    account_type = models.CharField(
+        max_length=3,
+        choices= ACCOUNT_TYPE_CHOICE ,
+        default=CHECKING,
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    user_id = models.ForeignKey("auth.User", verbose_name=_("Owner"), on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(_("Description"))
+    theme_color_hash = models.CharField(_("Theme Color"), max_length=7)
+
+    INCOME = "INC"
+    EXPENSE = "EXP"
+    TRANSFER = "TRN"
+    CATEGORY_TYPE_CHOICE = {
+        INCOME: "Income",
+        EXPENSE: "Expense",
+        TRANSFER: "Transfer",
+    }
+
+    category_type = models.CharField(
+        max_length=3,
+        choices=CATEGORY_TYPE_CHOICE,
+        default=EXPENSE,
+    )
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Category_detail", kwargs={"pk": self.pk})
+
+
+class Transaction(models.Model):
+    user_id = models.ForeignKey("auth.User", verbose_name=_("Owner"), on_delete=models.CASCADE)
+    account_id = models.ForeignKey("account_management_api.Account", verbose_name=_("Account"), on_delete=models.CASCADE)
+    category_id = models.ForeignKey("account_management_api.Category", verbose_name=_("Category"), on_delete=models.CASCADE)
+    amount = models.IntegerField(_("Amount"))
+    note = models.TextField(_("Note"))
+
+    class Meta:
+        verbose_name = _("Transaction")
+        verbose_name_plural = _("Transactions")
+
+    def __str__(self):
+        return str(self.amount) + " in " + str(account_id)
+
+    def get_absolute_url(self):
+        return reverse("Transaction_detail", kwargs={"pk": self.pk})
